3e0c97bd2374220919ca2a1d0f4d49c88a0a016c
prnvshrn <prnvshrn@gmail.com>
2019-01-22 02:18:13 -0800

    Added facebook microservice

---
+++ micro.py
@@ -0,0 +1,13 @@
+import facebook, urllib3, requests
+
+token = 'EAALBd0onih0BANOKHWOy1Vjc0YdVAvrP53tOlo1xDjH3upxJTnjTFQaqQIh5riDMrmksbPEPizcnZCfUf5sSolC9jZAnftthwWtZCni08E5JeBW7ChiXyY7bO6EgE1ar7vXxyPpyyn7qz7Le8ktVJVbr9NRL8OSMLmZCZBHD2B6enq9o8EgWRIlZC1cwvFZCjPvnRTt2uiGfAZDZD'
+
+def init():
+    graph = facebook.GraphAPI(access_token=token, version = 2.8)
+    #events = graph.request('/search?q=Poetry&type=event&limit=10000')
+    events = graph.request('/me/photos')
+    eventList = events['data']
+    print(events)
+
+if __name__ == '__main__':
+    init()
